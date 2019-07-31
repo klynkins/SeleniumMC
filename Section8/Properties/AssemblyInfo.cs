9b441f502ad4958ad32dcc00b70c5367a8a7677e
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-07-31 08:07:00 -0700

    Section 8

---
+++ Section8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section8")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ab2c872-572a-454b-a38a-97446677481c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Section8/WhileLoops.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Section8
+{
+    [TestClass]
+    public class WhileLoops
+    {
+        [TestMethod]
+        public void Test_While_Loops()
+        {
+            int number = 1;
+            while(number < 11)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+        }
+
+        [TestMethod]
+        public void Test_Operators()
+        {
+            int number = 1;
+            Console.WriteLine("post increment valus is: {0}", number++);
+            Console.WriteLine("pre increment valus is: {0}", ++number);
+            Console.WriteLine("post decrement valus is: {0}", number--);
+            Console.WriteLine("pre decrement valus is: {0}", --number);
+        }
+
+        [TestMethod]
+        public void Test_Sentinel_Controlled_Loop()
+        {
+            string inValue = "";
+            while (inValue != "-99")
+            {
+                Console.Write("\nEnter value (-99 to exit): ");
+                inValue = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+        [TestMethod]
+        public void Nested_Loops()
+        {
+            int number1 = 1;
+            int number2 = 1;
+            while(number1 <= 5)
+            {
+                Console.WriteLine(number2);
+                number2++;
+            }
+            number1++;
+        }
+    }
+    }
+
